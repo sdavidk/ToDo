01a8bac5fd40fc2c7c253ee5961eac5eef50163b
sdavidk <davidkohno500@gmail.com>
2016-08-30 11:30:52 -0400

    update

---
+++ ToDo/Controllers/ItemsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoEntities db = new ToDoEntities();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            var items = db.Items.Include(i => i.List);
+            return View(items.ToList());
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            ViewBag.ItemID = new SelectList(db.Lists, "ListID", "ListTitle");
+            return View();
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,ItemName,ListID,DueDateTime,Details,IsDone")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ItemID = new SelectList(db.Lists, "ListID", "ListTitle", item.ItemID);
+            return View(item);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ItemID = new SelectList(db.Lists, "ListID", "ListTitle", item.ItemID);
+            return View(item);
+            public ActionResult SetDone(int? id, bool newDoneValue);
+
+        }
+        public ActionResult ToggleDone(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+        
+            if (item.IsDone)
+            {
+                item.IsDone = false;
+            }
+            else
+            {
+                item.IsDone = true;
+            }
+                
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,ItemName,ListID,DueDateTime,Details,IsDone")] Item item)
+        {
+
+            ViewBag.ItemID = new SelectList(db.Lists, "ListID", "ListTitle", item.ItemID);
+            return View(item);
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ToDo/Controllers/ListsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class ListsController : Controller
+    {
+        private ToDoEntities db = new ToDoEntities();
+
+        // GET: Lists
+        public ActionResult Index()
+        {
+            var lists = db.Lists.Include(l => l.Item);
+            return View(lists.ToList());
+        }
+
+        // GET: Lists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            List list = db.Lists.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // GET: Lists/Create
+        public ActionResult Create()
+        {
+            ViewBag.ListID = new SelectList(db.Items, "ItemID", "ItemName");
+            return View();
+        }
+
+        // POST: Lists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ListID,ListTitle")] List list)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lists.Add(list);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ListID = new SelectList(db.Items, "ItemID", "ItemName", list.ListID);
+            return View(list);
+        }
+
+        // GET: Lists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            List list = db.Lists.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ListID = new SelectList(db.Items, "ItemID", "ItemName", list.ListID);
+            return View(list);
+        }
+
+        // POST: Lists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ListID,ListTitle")] List list)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(list).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ListID = new SelectList(db.Items, "ItemID", "ItemName", list.ListID);
+            return View(list);
+        }
+
+        // GET: Lists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            List list = db.Lists.Find(id);
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // POST: Lists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            List list = db.Lists.Find(id);
+            db.Lists.Remove(list);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
